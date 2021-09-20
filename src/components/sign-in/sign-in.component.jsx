197009eba6f28744d04ddf904a28a41a4ebe30ec
Nishant Kedar <nkedar@outlook.com>
2021-09-20 23:56:38 +0530

    cart icon

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,9 +4,8 @@ import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super();
     this.state = {
       email: '',
       password: ''
